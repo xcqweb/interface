d54bce428af051e32385b66daa09cdcd816a6415
unknown <linc.lin@getech.cn>
2019-06-21 15:12:39 +0800

    时间显示NaN

--- src/static/js/Preview.js
+++ src/static/js/Preview.js
@@ -103,8 +103,8 @@ function createWs(pageId) {
     return;
   };
   const token = getCookie('token');
-  // let ws = new WebSocket(`ws://${location.host}/ws/websocket`, token); // 提交时使用这个
-  let ws = new WebSocket(`ws://10.74.20.17:8082/websocket`, token); // SIT环境websocket,调试用这个
+  let ws = new WebSocket(`ws://${location.host}/ws/websocket`, token); // 提交时使用这个
+  // let ws = new WebSocket(`ws://10.74.20.17:8082/websocket`, token); // SIT环境websocket,调试用这个
   initialWs(ws, pageId);
   return ws;
 }
