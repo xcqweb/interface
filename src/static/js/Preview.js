b4742f71426aad6554e94721a2304c9dd858968b
unknown <linc.lin@getech.cn>
2019-06-25 16:17:19 +0800

    bug dix

--- src/static/js/Preview.js
+++ src/static/js/Preview.js
@@ -103,8 +103,8 @@ function createWs(pageId) {
     return;
   };
   const token = getCookie('token');
-  // let ws = new WebSocket(`ws://${location.host}/ws/websocket`, token); // 提交时使用这个
-  let ws = new WebSocket(`ws://10.74.20.17:8082/websocket`, token); // SIT环境websocket,调试用这个
+  let ws = new WebSocket(`ws://${location.host}/ws/websocket`, token); // 提交时使用这个
+  // let ws = new WebSocket(`ws://10.74.20.17:8082/websocket`, token); // SIT环境websocket,调试用这个
   initialWs(ws, pageId);
   return ws;
 }
@@ -156,7 +156,6 @@ function initialWs (ws, pageId) {
         }
       }
       if (resData.operation === 3) {
-        console.log(111, doms);
         for (let dom of doms) {
           if (dom.childElementCount == 0) {
             dom.style.backgroundColor = dom.getAttribute('data-defaultFill');
