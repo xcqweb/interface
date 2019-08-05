995c6dfa378f081d92764178ac7f2d295405c5f9
杜青 <qing2.du@getech.cn>
2019-08-05 16:07:15 +0800

    改造rightBar  1

---
+++ src/views/rightBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="geSidebarContainer geRightBarContainer" />
+</template>
+<script>
+// import {mxUtils} from '../services/mxGlobal'
+export default{
+    components:{
+    },
+    props:[''],
+    data() {
+        return {
+        }
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style lang="less">
+.geRightBarContainer{
+}
+
+</style>
