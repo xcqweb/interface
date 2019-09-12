b9ad45224e159a0dee3cf3e049212749b2d94cb0
new <new@yzqMacBook-Air.local>
2019-09-12 11:38:10 +0800

    bug修复

--- src/views/datasource/dataSource-right.vue
+++ src/views/datasource/dataSource-right.vue
@@ -358,21 +358,26 @@ export default {
             }).then((res) => {
                 const [firstParamNameList, firstDeviceNameList] = res
                 this.paramsNameList = firstParamNameList.records || []
-                // VueEvent.$emit('StartparamsNameArr', this.paramsNameList)
                 this.deviceNameList = firstDeviceNameList.records || []
                 this.paramListTotal = firstParamNameList.total || 10
                 this.deviceListTotal = firstDeviceNameList.total || 10
-                // 要拿所有数据参数
-                let objDataNew2 = {
-                    studioId:this.studioIdNew,
-                    deviceTypeId: this.deviceTypeArr[0].deviceTypeId,
-                    current: this.PAGE_CURRENT,
-                    size:100000,
-                    type:1
+                // 要拿所有数据参数 当参数第一页多余10条
+                if (this.paramsNameList.length >= this.PAGE_SIZE) {
+                    
+                    let objDataNew2 = {
+                        studioId:this.studioIdNew,
+                        deviceTypeId: this.deviceTypeArr[0].deviceTypeId,
+                        current: this.PAGE_CURRENT,
+                        size:100000,
+                        type:1
+                    }
+                    this.requestUtil.post(this.urls.deviceParamList.url, objDataNew2).then((res) => {
+                        VueEvent.$emit('StartparamsNameArr', res.records || [])
+                    }).catch(() => {
+                        Message.error('系统繁忙，请稍后再试试')
+                        return false
+                    })
                 }
-                this.requestUtil.post(this.urls.deviceParamList.url, objDataNew2).then((res) => {
-                    VueEvent.$emit('StartparamsNameArr', res.records || [])
-                })
             }).catch(() => {
                 Message.error('系统繁忙，请稍后再试试')
                 return false
@@ -394,16 +399,7 @@ export default {
                     size:this.PAGE_SIZE,
                     type:1
                 }
-                let objDataNew2 = {
-                    studioId:this.studioIdNew,
-                    deviceTypeId: deviceTypeId,
-                    current: this.PAGE_CURRENT,
-                    size:100000,
-                    type:1
-                }
-                this.requestUtil.post(this.urls.deviceParamList.url, objDataNew2).then((res) => {
-                    VueEvent.$emit('StartparamsNameArr', res.records || [])
-                })
+                
                 const [ParamNameList, DeviceNameList] = await Promise.all([
                     this.requestUtil.post(this.urls.deviceParamList.url, objDataNew),
                     this.requestUtil.post(this.urls.deviceEquipList.url, objDataNew)
@@ -415,6 +411,24 @@ export default {
                 this.deviceNameList = DeviceNameList.records || []
                 this.paramListTotal = ParamNameList.total || 10
                 this.deviceListTotal = DeviceNameList.total || 10
+
+                // 
+                if (this.paramsNameList.length >= this.PAGE_SIZE) {
+                    let objDataNew2 = {
+                        studioId:this.studioIdNew,
+                        deviceTypeId: deviceTypeId,
+                        current: this.PAGE_CURRENT,
+                        size:100000,
+                        type:1
+                    }
+                    this.requestUtil.post(this.urls.deviceParamList.url, objDataNew2).then((res) => {
+                        VueEvent.$emit('StartparamsNameArr', res.records || [])
+                    }).catch(() => {
+                        Message.error('系统繁忙，请稍后再试')
+                        return false
+                    })
+
+                }
                 
             }
         },
