6bc7d43a5f6c71302bd907a717ffa2379e98fe04
lishihong <546537081@qq.com>
2019-12-30 20:47:29 +0800

    坐标轴名称优化

--- src/services/preview/util.js
+++ src/services/preview/util.js
@@ -376,7 +376,7 @@ function dealCharts(mainProcess,cell) {
                 if (cell.shapeName == 'lineChart') {
                     let tempOptions = JSON.parse(JSON.stringify(options))
                     tempOptions.xAxis.data = []
-                    tempOptions.yAxis.name = titleShow
+                    tempOptions.yAxis.name = `                       ${titleShow}`
                     tempOptions.legend.data = []
                     let tempLegend = [], tempSeries = []
                     let markLine = tempOptions.series[0].markLine
