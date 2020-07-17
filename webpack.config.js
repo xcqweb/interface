423b47158b1db661857da6bba3baa4625345c82c
zhiqiang.yao <zhiqiang.yao@tcl.com>
2020-07-17 15:58:38 +0800

    修改代理

--- webpack.config.js
+++ webpack.config.js
@@ -142,7 +142,7 @@ module.exports = {
     proxy: {
       "/api": {
         //target: "http://10.74.20.25",
-        target: 'http://kong-ingress.ele-uat.10.74.152.23.xip.io/', //tcl sit
+        target: 'http://kong.ele-iot.10.74.158.69.nip.io/', //tcl sit
         // target: 'http://kong.csot-iot-sit.10.74.20.12.nip.io/',
         changeOrigin: true,
         // "pathRewrite": {"^/api": "/api"}
