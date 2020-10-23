edac7c2493fe7f8cd58fc2528efd50040c679e87
changqian.xu@getech.cn <changqian.xu@getech.cn>
2020-10-23 18:22:25 +0800

    add

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,5 @@
 let prefix = `${window.API_PREFI}`;
+console.log(prefix)
 export default {
   testBaseUrl: {url: "", "desc": "测试环境"},
   baseUrl: {url: `${window.location.origin}/`, "desc": "生产环境"},
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,8 @@ import store from '../store/'
 const isDev = process.env.NODE_ENV === 'development'
 const isUat = true
 window.PREFIX_PATH = isDev ? '' : isUat ? '/iot/interface' : '/interface'
-window.API_PREFIX = isUat ? 'iot' : ''
+// window.PREFIX_PATH = '/iot/interface'
+window.API_PREFIX = isUat ? '/iot' : ''
 
 Vue.use(Router)
 
