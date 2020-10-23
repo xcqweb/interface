506c88dff52723e587bffe6b008a384cb62ded3b
changqian.xu@getech.cn <changqian.xu@getech.cn>
2020-10-23 18:10:05 +0800

    edit

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,4 @@
-let prefix = `${window.API_PREFI}/api`;
+let prefix = `${window.API_PREFI}`;
 export default {
   testBaseUrl: {url: "", "desc": "测试环境"},
   baseUrl: {url: `${window.location.origin}/`, "desc": "生产环境"},
@@ -32,12 +32,12 @@ export default {
   newDeviceModelList: {url: `${prefix}/api/device/deviceModel/select`, 'desc': '设备型号'},
   newDeviceTypeList: {url: `${prefix}/api/device/deviceType/child`, 'desc': '查询位置节点的子节点_设备分类'},
   newDeviceTemplateLIST: {url: `${prefix}/api/device/deviceModel/page`, 'desc': '根据类型获取模版列表'},
-  newAppDataList: {url: `${prefix}/iot-cds/cds/findConfigApp`, 'desc': '导入数据源获取应用数据源(组态工具V2.0)'},
-  newImportDsApi: {url: `${prefix}/iot-cds/cds/configDevice`, 'desc': '导入数据源确认接口'},
-  newImportDeviceList: {url: `${prefix}/iot-cds/cds/dataSource/selectDataImport`, 'desc': '设备模版下的设备'},
-  newImportApp: {url: `${prefix}/iot-cds/cds/findImportApp`, 'desc': '获取已导入的数据源应用'},
-  newApplyParams: {url: `${prefix}/iot-cds/cds/findConfigAppParam`, 'desc': '数据源获取应用下参数'},
+  newAppDataList: {url: `${prefix}/api/iot-cds/cds/findConfigApp`, 'desc': '导入数据源获取应用数据源(组态工具V2.0)'},
+  newImportDsApi: {url: `${prefix}/api/iot-cds/cds/configDevice`, 'desc': '导入数据源确认接口'},
+  newImportDeviceList: {url: `${prefix}/api/iot-cds/cds/dataSource/selectDataImport`, 'desc': '设备模版下的设备'},
+  newImportApp: {url: `${prefix}/api/iot-cds/cds/findImportApp`, 'desc': '获取已导入的数据源应用'},
+  newApplyParams: {url: `${prefix}/api/iot-cds/cds/findConfigAppParam`, 'desc': '数据源获取应用下参数'},
   // 20200720
-  getDataSource: {url: `${prefix}/device/device/partSource`, 'desc': '获取数据源接口'}, 
-  newDeleteApply: {url: `${prefix}/iot-cds/cds/delConfigApp`, 'desc': '数据源移除应用'},
+  getDataSource: {url: `${prefix}/api/device/device/partSource`, 'desc': '获取数据源接口'}, 
+  newDeleteApply: {url: `${prefix}/api/iot-cds/cds/delConfigApp`, 'desc': '数据源移除应用'},
 }
