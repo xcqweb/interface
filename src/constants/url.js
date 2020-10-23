e988db3c00e537b28a96801ea97827dbe7688ce0
changqian.xu@getech.cn <changqian.xu@getech.cn>
2020-10-23 17:34:23 +0800

    url.js

--- src/constants/url.js
+++ src/constants/url.js
@@ -25,9 +25,9 @@ export default {
   normalParam: {url: 'api/device/deviceModel/param/normal/select', 'desc': '获取设备参数'},
   fictitiousParam: {url: 'api/device/deviceModel/param/fictitious/select', 'desc': '获取虚拟参数'},
   deviceParamGenerate: {url: 'api/device/index/deviceParams', 'desc': '生成拼接的设备参数接口'},
-  commandTemplate: {url: '/api/device/deviceModel/command/', 'desc': '根据设备模板id查询指令模板'},
-  commandSend: {url: '/api/command/center/send', 'desc': '下发指令'},
-  commandTplVariable: {url: '/api/device/deviceModel/command/getVariable', 'desc': '根据指令模板id查询对应的属性变量'},
+  commandTemplate: {url: 'api/device/deviceModel/command/', 'desc': '根据设备模板id查询指令模板'},
+  commandSend: {url: 'api/command/center/send', 'desc': '下发指令'},
+  commandTplVariable: {url: 'api/device/deviceModel/command/getVariable', 'desc': '根据指令模板id查询对应的属性变量'},
   // 20200601
   newDeviceModelList: {url: `api/device/deviceModel/select`, 'desc': '设备型号'},
   newDeviceTypeList: {url: `api/device/deviceType/child`, 'desc': '查询位置节点的子节点_设备分类'},
